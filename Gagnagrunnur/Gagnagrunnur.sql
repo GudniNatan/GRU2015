4228b54c1099828ed55926b0bacd7a2175f2d24b
Guðni Natan Gunnarsson <gudni.nathan@gmail.com>
2015-11-02 11:53:58 +0000

    Breytti Gagnagrunnur.sql
    Bætti við sample gögnum.

--- Gagnagrunnur/Gagnagrunnur.sql
+++ Gagnagrunnur/Gagnagrunnur.sql
@@ -8,7 +8,7 @@ USE 1803982879_dub16;
 CREATE TABLE Medlimur(
 ID INT(3) PRIMARY KEY AUTO_INCREMENT NOT NULL,
 nafn VARCHAR(255),
-kennitala CHAR(10),
+kennitala CHAR(10) UNIQUE KEY,
 simi CHAR(7)
 );
 
@@ -25,8 +25,28 @@ FOREIGN KEY (medlimur_ID) REFERENCES Medlimur(ID)
 );
 
 CREATE TABLE Skraning(
+ID INT(3) PRIMARY KEY AUTO_INCREMENT NOT NULL,
 vidburdur_id INT(3) NOT NULL,
 medlimur_id INT(3) NOT NULL,
 FOREIGN KEY (vidburdur_id) REFERENCES Vidburdur(ID),
 FOREIGN KEY (medlimur_ID) REFERENCES Medlimur(ID)
 );
+
+/* Sample gögn */
+INSERT INTO Vidburdur(heiti, dagsetning)
+VALUES
+('Íspartý', '2015-11-25'),
+('Star Wars Bíóferð', '2015-12-7'),
+('JólaLAN', '2015-12-27');
+
+INSERT INTO Medlimur(nafn, kennitala, simi)
+VALUES
+('Guðni Natan Gunnarsson', '1803982879', '6952452');
+
+INSERT INTO Admin(medlimur_ID)
+VALUES
+(1);
+
+INSERT INTO Skraning(vidburdur_id, medlimur_id)
+VALUES
+(1, 1);
